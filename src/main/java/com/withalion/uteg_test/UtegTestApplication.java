1e141959ac53035b827c3212d6ffa8d800d9ab33
Withalion <bagarmatej@gmail.com>
2024-10-09 14:43:07 +0200

    Initialize Spring Boot project

---
+++ src/main/java/com/withalion/uteg_test/UtegTestApplication.java
@@ -0,0 +1,13 @@
+package com.withalion.uteg_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtegTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtegTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/withalion/uteg_test/UtegTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withalion.uteg_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtegTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
